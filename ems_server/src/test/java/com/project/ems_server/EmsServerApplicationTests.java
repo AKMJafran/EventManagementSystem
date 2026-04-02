8fa738ad2bf3a95b6c048b834666ceeb065c8c90
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2026-04-02 12:11:35 +0530

    hghghj

--- ems_server/src/test/java/com/project/ems_server/EmsServerApplicationTests.java
+++ ems_server/src/test/java/com/project/ems_server/EmsServerApplicationTests.java
@@ -2,12 +2,25 @@ package com.project.ems_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class EmsServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	static class TestConfig {
+
+		@Bean
+		public JavaMailSender javaMailSender() {
+			return new JavaMailSenderImpl();
+		}
 	}
 
 }
